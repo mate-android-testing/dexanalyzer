46eaee29c0b0a3bc8639f95c3e53854f01bdd400
Michael <auermich@fim.uni-passau.de>
2023-06-06 18:52:00 +0200

    removed unused import

--- src/main/java/de/uni_passau/fim/auermich/android_analysis/Main.java
+++ src/main/java/de/uni_passau/fim/auermich/android_analysis/Main.java
@@ -20,7 +20,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
